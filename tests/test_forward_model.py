30370da79e94353317181934dfb2b3f145f498ce
davidcortesortuno <david.cortes.o@gmail.com>
2020-11-17 16:09:13 +0100

    Documented test of FW model. Added simple test to inversion lib

--- tests/test_forward_model.py
+++ tests/test_forward_model.py
@@ -2,7 +2,12 @@ import numpy as np
 import multipole_inversion.magnetic_sample as msp
 
 
-def test_grid():
+def test_fw_model_and_bz_field():
+    """
+    Test for the creation of a FW model from the MagneticSample class
+    The Bz field is tested at a specific point of the scan grid by comparing
+    the simulation to the theoretical value
+    """
 
     Hz = 1e-6       # Scan height in m
     Sx = 5e-6       # Scan area x - dimension in m
@@ -15,7 +20,9 @@ def test_grid():
 
     # Initialise the dipole class
     sample = msp.MagneticSample(Hz, Sx, Sy, Sdx, Sdy, Lx, Ly, Lz,
-                                scan_origin=(0.5e-6, 0.5e-6))
+                                scan_origin=(0.5e-6, 0.5e-6),
+                                bz_field_module='spherical_harmonics_basis'
+                                )
 
     # Manually set the positions and magnetization of the two dipoles
     Ms = 1e5
@@ -25,6 +32,7 @@ def test_grid():
     volumes = np.array([1e-18])
     sample.generate_particles_from_array(dipole_positions, magnetization, volumes)
 
+    # Test components of the dipole moment are passed correctly:
     assert abs(sample.dipole_moments[0][0] - 1e-13) < 1e-16
     assert abs(sample.dipole_moments[0][1] - 0.0) < 1e-16
     assert abs(sample.dipole_moments[0][2] - 0.0) < 1e-16
@@ -35,14 +43,18 @@ def test_grid():
     # measurement surface
     sample.generate_measurement_mesh()
 
+    # Check that the scan grid has the right dimensions
     assert len(sample.Sx_range) == 5
     assert len(sample.Sy_range) == 5
     print(sample.Sx_range)
 
+    # Analytic calculations ---------------------------------------------------
     # mu0 = 4 * np.pi * 1e-7
-    # Theoretical field at
+    # Theoretical field computed at the point in the scan grid:
     rx, ry, rz = 3.5e-6, 3.5e-6, 1e-6
+    # This is the position of the dipole center:
     x, y, z = 2.5e-6, 2.5e-6, -2.5e-6
+    # We need the difference
     dx, dy, dz = rx - x, ry - y, rz - z
     r2 = dx ** 2 + dy ** 2 + dz ** 2
     r = np.sqrt(r2)
@@ -55,14 +67,19 @@ def test_grid():
     g = -1e-7 / (r2 * r)
     # Bz field
     bz_field = f * dz + g * mz
+    # -------------------------------------------------------------------------
 
-    print(np.array2string(sample.Bz_array, precision=5))
-    print(bz_field)
+    # Now test the theoretical field with the one in the FW model simulation
+    # print(np.array2string(sample.Bz_array, precision=5))
+    # print(bz_field)
     # print('{:.30f}'.format(abs(bz_field - sample.Bz_array[3, 3])))
     assert abs(bz_field - sample.Bz_array[3, 3]) < 1e-20
 
-    # print(sample.)
+
+# TODO:
+# Test random generation of dipole sources
+# Test generation of noise in sample
 
 
 if __name__ == '__main__':
-    test_grid()
+    test_fw_model_and_bz_field()
---
+++ tests/test_inversion.py
@@ -0,0 +1,75 @@
+import numpy as np
+import multipole_inversion.magnetic_sample as msp
+import multipole_inversion.multipole_inversion as minv
+from pathlib import Path
+
+
+TEST_SAVEDIR = Path('TEST_TMP')
+TEST_SAVEDIR.mkdir(exist_ok=True)
+
+
+def fw_model_fun():
+    """
+    """
+
+    Hz = 1e-6       # Scan height in m
+    Sx = 20e-6      # Scan area x - dimension in m
+    Sy = 20e-6      # Scan area y - dimension in m
+    Sdx = 1e-6      # Scan x - step in m
+    Sdy = 1e-6      # Scan y - step in m
+    Lx = Sx * 1.0   # Sample x - dimension in m
+    Ly = Sy * 1.0   # Sample y - dimension in m
+    Lz = 10e-6      # Sample thickness in m
+
+    # Initialise the dipole class
+    sample = msp.MagneticSample(Hz, Sx, Sy, Sdx, Sdy, Lx, Ly, Lz,
+                                scan_origin=(0e-6, 0e-6),
+                                bz_field_module='spherical_harmonics_basis'
+                                )
+
+    # Manually set the positions and magnetization of the two dipoles
+    dipole_positions = np.array([[sample.Lx * 0.5, sample.Ly * 0.5, -sample.Lz * 0.5]])
+
+    Ms = 1e5
+    orientation = np.array([1., 0., 1.])
+    orientation /= np.linalg.norm(orientation)
+    magnetization = Ms * (1 * 1e-18) * np.array([orientation])
+    volumes = np.array([1e-18])
+    sample.generate_particles_from_array(dipole_positions,
+                                         magnetization,
+                                         volumes)
+
+    # Generate the dipole field measured as the Bz field flux through the
+    # measurement surface
+    sample.generate_measurement_mesh()
+
+    sample.save_data(filename='fw_model_test_inversion', basedir=TEST_SAVEDIR)
+
+
+def test_inversion_single_dipole():
+
+    # Generate arrays from the Forward model using a single dipole source
+    fw_model_fun()
+
+    inv_model = minv.MultipoleInversion(
+            TEST_SAVEDIR / 'MetaDict_fw_model_test_inversion.json',
+            TEST_SAVEDIR / 'MagneticSample_fw_model_test_inversion.npz',
+            expansion_limit='quadrupole',
+            sus_functions_module='spherical_harmonics_basis'
+            )
+    inv_model.generate_measurement_mesh()
+    inv_model.compute_inversion()
+
+    Ms = 1e5
+    orientation = np.array([1., 0., 1.])
+    orientation /= np.linalg.norm(orientation)
+    expected_magnetization = Ms * (1 * 1e-18) * orientation
+
+    # print(inv_model.inv_multipole_moments[0])
+    for i in range(3):
+        assert abs(expected_magnetization[i] -
+                   inv_model.inv_multipole_moments[0][i]) < 1e-22
+
+
+if __name__ == '__main__':
+    test_inversion_single_dipole()
