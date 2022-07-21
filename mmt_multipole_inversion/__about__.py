45c19a74a569f5fa881d05d69c6c20fe64518c24
David Cortes Ortuno <david.co@mailbox.org>
2022-07-21 12:34:36 +0200

    Adding metadata in about file. Updating to 1.0 version

---
+++ mmt_multipole_inversion/__about__.py
@@ -0,0 +1,25 @@
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+__title__ = "mmt_multipole_inversion"
+__summary__ = (
+    "Library to generate scan grid measurements from multipole sources and "
+    "perform numerical inversions"
+)
+__uri__ = "https://github.com/Micromagnetic-Tomography/mmt_multipole_inversion"
+
+__version__ = "1.0"
+
+__author__ = "D. Cortés-Ortuño, K. Fabian, L. V. de Groot"
+__email__ = "d.i.cortes@uu.nl"
+
+__license__ = "MIT"
+__copyright__ = "Copyright 2022 {}".format(__author__)
