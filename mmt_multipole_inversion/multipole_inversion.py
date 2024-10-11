Fixed torchmin minimization by scaling units. Working but requires further testing and proper set of initial state

mmt_multipole_inversion/multipole_inversion.py
@@ -420,8 +420,8 @@ class MultipoleInversion(object):
             # x0 = np.ones(self.N_particles * self._N_cols)
             
             # Nelder-Mead working fine:
-            minResult = so.minimize(minF, x0, tol=1e-20, options=dict(maxiter=10, disp=True),
-                                    method='Nelder-Mead')
+            # minResult = so.minimize(minF, x0, tol=1e-20, options=dict(maxiter=10, disp=True),
+            #                         method='Nelder-Mead')
 
             # CG or gradient-dependent method have problems with jacobian calculation:
             minResult = so.minimize(minF, minResult.x, tol=1e-25, jac='cs', # options=dict(gtol=1e-25, xrtol=1e-20, disp=True),
@@ -463,11 +463,19 @@ class MultipoleInversion(object):
             # if self._expansion_limit == 'quadrupole':
             #     x0[3:] = 1e-18
 
+            # TODO: Update minimization tolerances and options
             # minResult = tmin.minimize(minF, x0, options=dict(xtol=1e-30, gtol=1e-25, line_search='strong-wolfe', disp=2), method='bfgs', disp=2)
             minResult = tmin.minimize(minF, x0, options=dict(xtol=1e-25, gtol=1e-25, line_search='strong-wolfe', normp=2, disp=2), method='l-bfgs', disp=2)
             # minResult = tmin.minimize(minF, x0, options=dict(gtol=1e-25, disp=2), method='cg', disp=2)
 
             self.inv_multipole_moments = minResult.x.numpy()
+            # Scale the magnetic moments back to meter units:
+            µm = 1e-6
+            self.inv_multipole_moments[:3] *= µm**3
+            if self.expansion_limit in ['quadrupole', 'octupole']:
+                self.inv_multipole_moments[3:8] *= µm**5
+            if self.expansion_limit in ['octupole']:
+                self.inv_multipole_moments[8:15] *= µm**7
             self.inv_multipole_moments.shape = (self.N_particles, self._N_cols)
 
             inv_Bz_array, _ = Bflux_residual_f(minResult.x, tBz.flatten(), self.N_sensors, self._N_cols, self.N_particles, tParticlePositions,

mmt_multipole_inversion/susceptibility_modules/torchm/torch_functions.py
@@ -1,10 +1,15 @@
 import numpy as np
 import torch
 
+# NOTE: To make Torch minimization happy, using a larger scale for the field and
+# the space dimensions, we will scale position, locally in this file, Using
+# micrometers and nano Tesla units
+µm = 1e6
+nT = 1e9
 
 def dipole_Bz_sus(xin, N_sensors, N_particles, dip_r, pos_r, yout, n_col_stride):
     for i in range(N_sensors):
-        dr = pos_r[i] - dip_r
+        dr = µm *(pos_r[i] - dip_r)
         x, y, z = dr[:, 0], dr[:, 1], dr[:, 2]
         z2 = z ** 2
 
@@ -13,28 +18,29 @@ def dipole_Bz_sus(xin, N_sensors, N_particles, dip_r, pos_r, yout, n_col_stride)
         f = 1e-7 / (r2 * r2 * r)
 
         #  Only return Bz
-        yout[i] += torch.dot(f * (3 * x * z), xin[::n_col_stride])
-        yout[i] += torch.dot(f * (3 * y * z), xin[1::n_col_stride])
-        yout[i] += torch.dot(f * (3 * z2 - r2), xin[2::n_col_stride])
+        yout[i] += nT * torch.dot(f * (3 * x * z), xin[::n_col_stride])
+        yout[i] += nT * torch.dot(f * (3 * y * z), xin[1::n_col_stride])
+        yout[i] += nT * torch.dot(f * (3 * z2 - r2), xin[2::n_col_stride])
 
     return None
 
 
 def quadrupole_Bz_sus(xin, N_sensors, N_particles, dip_r, pos_r, yout, n_col_stride):
     for i in range(N_sensors):
-        dr = pos_r[i] - dip_r
+        dr = µm * (pos_r[i] - dip_r)
         x, y, z = dr[:, 0], dr[:, 1], dr[:, 2]
         x2, y2, z2 = x ** 2, y ** 2, z ** 2
 
-        r2 = torch.sum(dr ** 2, dim=1)
+        # r2 = torch.sum(dr ** 2, dim=1)
+        r2 = x2 + y2 + z2
         r = np.sqrt(r2)
         g = 1e-7 / (r2 * r2 * r2 * r)
-
-        yout[i] += torch.dot(g * np.sqrt(3 / 2) * z * (-3 * r2 + 5 * z2), xin[3::n_col_stride])
-        yout[i] += torch.dot(g * -np.sqrt(2) * x * (r2 - 5 * z2), xin[4::n_col_stride])
-        yout[i] += torch.dot(g * -np.sqrt(2) * y * (r2 - 5 * z2), xin[5::n_col_stride])
-        yout[i] += torch.dot(g * (5 / np.sqrt(2)) * (x2 - y2) * z, xin[6::n_col_stride])
-        yout[i] += torch.dot(g * 5 * np.sqrt(2) * x * y * z, xin[7::n_col_stride])
+        
+        yout[i] += nT * torch.dot(g * np.sqrt(3 / 2) * z * (-3 * r2 + 5 * z2), xin[3::n_col_stride])
+        yout[i] += nT * torch.dot(g * -np.sqrt(2) * x * (r2 - 5 * z2), xin[4::n_col_stride])
+        yout[i] += nT * torch.dot(g * -np.sqrt(2) * y * (r2 - 5 * z2), xin[5::n_col_stride])
+        yout[i] += nT * torch.dot(g * (5 / np.sqrt(2)) * (x2 - y2) * z, xin[6::n_col_stride])
+        yout[i] += nT * torch.dot(g * 5 * np.sqrt(2) * x * y * z, xin[7::n_col_stride])
 
     return None
 
@@ -43,7 +49,7 @@ def quadrupole_Bz_sus(xin, N_sensors, N_particles, dip_r, pos_r, yout, n_col_str
 def octupole_Bz_sus(xin, N_sensors, N_particles,
                     dip_r, pos_r, yout, n_col_stride):
     for i in range(N_sensors):
-        dr = pos_r[i] - dip_r
+        dr = µm * (pos_r[i] - dip_r)
         x, y, z = dr[:, 0], dr[:, 1], dr[:, 2]
         x2, y2, z2 = x ** 2, y ** 2, z ** 2
 
@@ -53,21 +59,15 @@ def octupole_Bz_sus(xin, N_sensors, N_particles,
         g = 1e-7 / (r4 * r4 * r)
 
         # Fill the Q array using n_col_stride = 8
-        yout[i] += torch.dot(g * (3 * (r2 ** 2) - 30 * r2 * z2
+        yout[i] += nT * torch.dot(g * (3 * (r2 ** 2) - 30 * r2 * z2
                                + 35 * (z2 * z2)) / np.sqrt(10),
-                          xin[8*N_particles:9*N_particles])
-        yout[i] += torch.dot(g * np.sqrt(15) * x * z * (-3 * r2 + 7 * z2) / 2,
-                          xin[9*N_particles:10*N_particles])
-        yout[i] += torch.dot(g * np.sqrt(15) * y * z * (-3 * r2 + 7 * z2) / 2,
-                          xin[10*N_particles:11*N_particles])
-        yout[i] += torch.dot(g * -np.sqrt(1.5) * (x2 - y2) * (r2 - 7 * z2),
-                          xin[11*N_particles:12*N_particles])
-        yout[i] += torch.dot(g * -np.sqrt(6) * x * y * (r2 - 7 * z2),
-                          xin[12*N_particles:13*N_particles])
-        yout[i] += torch.dot(g * 7 * x * (x2 - 3 * y2) * z / 2,
-                          xin[13*N_particles:14*N_particles])
-        yout[i] += torch.dot(g * -7 * y * (-3 * x2 + y2) * z / 2,
-                          xin[14*N_particles:15*N_particles])
+                          xin[8::n_col_stride])
+        yout[i] += nT * torch.dot(g * np.sqrt(15) * x * z * (-3 * r2 + 7 * z2) / 2, xin[9::n_col_stride])
+        yout[i] += nT * torch.dot(g * np.sqrt(15) * y * z * (-3 * r2 + 7 * z2) / 2, xin[10::n_col_stride])
+        yout[i] += nT * torch.dot(g * -np.sqrt(1.5) * (x2 - y2) * (r2 - 7 * z2), xin[11::n_col_stride])
+        yout[i] += nT * torch.dot(g * -np.sqrt(6) * x * y * (r2 - 7 * z2), xin[12::n_col_stride])
+        yout[i] += nT * torch.dot(g * 7 * x * (x2 - 3 * y2) * z / 2, xin[13::n_col_stride])
+        yout[i] += nT * torch.dot(g * -7 * y * (-3 * x2 + y2) * z / 2, xin[14::n_col_stride])
 
     return None
 
@@ -89,8 +89,8 @@ def Bflux_residual_f(xin, Bdata, N_sensors, N_cols, N_particles, particle_positi
         if expansion_limit in ['octupole']:
             octupole_Bz_sus(xin, N_sensors, N_particles, particle_positions, scan_positions, yout, N_cols)
 
-    # the misfit functional that is minimised:
-    mf = torch.sum((yout - Bdata) ** 2)
+    # The misfit functional that is minimised in Tesla units
+    mf = torch.sum((yout - nT * Bdata) ** 2)
 
     # Using torch's norm:
     # mf = torch.norm(yout - Bdata, p=torch.inf)

tests/test_inversion_torch.py
@@ -39,7 +39,7 @@ def fw_model_fun(sensor_dx=1e-6, sensor_dy=1e-6, overwrite=False,
     # Manually set the positions and magnetization of the two dipoles
     dipole_positions = np.array([[sample.Lx * 0.5,
                                   sample.Ly * 0.5,
-                                  -sample.Lz * 0.5]])
+                                  -sample.Lz * 0.05]])
 
     Ms = 1e5
     orientation = np.array([1., 0., 1.])
@@ -72,6 +72,7 @@ def test_inversion_single_dipole_torch(limit):
         expansion_limit=limit,
         sus_functions_module='spherical_harmonics_basis')
     inv_model.generate_measurement_mesh()
+    # inv_model.compute_inversion(method='torchmin')
     inv_model.compute_inversion(method='torchmin')
     print(inv_model.Q)
 
@@ -98,8 +99,11 @@ def test_inversion_single_dipole_torch(limit):
     print(inv_model.inv_Bz_array.flatten()[:10])
     print(inv_model.Bz_array.flatten()[:10])
     f, axs = plt.subplots(ncols=2)
-    axs[0].imshow(inv_model.inv_Bz_array, origin='lower', cmap='RdYlBu')
-    axs[1].imshow(inv_model.Bz_array, origin='lower', cmap='RdYlBu')
+    lim = 1.7e-3 * 1e9
+    print('LIMS', np.min(inv_model.Bz_array), np.max(inv_model.Bz_array))
+    axs[0].imshow(inv_model.inv_Bz_array, origin='lower', cmap='RdYlBu', vmin=-lim, vmax=lim)
+    axs[1].imshow(inv_model.Bz_array * 1e9, origin='lower', cmap='RdYlBu', vmin=-lim, vmax=lim)
+    axs[1].set_title('Original')
     plt.show()
 
 
@@ -107,4 +111,4 @@ if __name__ == '__main__':
     fw_model_fun(overwrite=True)
 
     # test_inversion_single_dipole_torch(limit='dipole')
-    test_inversion_single_dipole_torch(limit='quadrupole')
+    test_inversion_single_dipole_torch(limit='octupole')